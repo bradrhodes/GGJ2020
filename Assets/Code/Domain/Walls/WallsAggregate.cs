04ec762e6f69687532301db207ff0d4e29853e5f
Brad R <1196817+bradrhodes@users.noreply.github.com>
2020-02-01 17:01:09 -0600

    Added the Walls domain and handled click events and emit a repaired event

---
+++ Assets/Code/Domain/Walls/WallsAggregate.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+public class WallsAggregate
+{
+	private Subject<WallsEvent> _events = new Subject<WallsEvent>();
+	private Dictionary<MapCoordinate, Wall> _walls = new Dictionary<MapCoordinate, Wall>();
+
+	public IObservable<WallsEvent> Events => _events;
+
+	public void Initialize(params InitialWall[] walls)
+    {
+        _walls = walls.ToDictionary(wall => wall.Coordinate, wall => new Wall(wall.Coordinate, WallState.Broken));
+        Emit(new WallsEvent.Initialized(walls));
+	}
+
+	private void Emit(WallsEvent @event)
+		=> _events.OnNext(@event);
+
+    public void Repair(MapCoordinate clickedCoordinate)
+    {
+        if (!_walls.ContainsKey(clickedCoordinate))
+            return;
+
+        var clickedWall = _walls.First(pair => pair.Key == clickedCoordinate).Value;
+
+		Emit(new WallsEvent.WallRepaired(clickedWall.Coordinate));
+    }
+}
+
+public class InitialWall
+{
+	public InitialWall(MapCoordinate coordinate)
+	{
+		Coordinate = coordinate;
+	}
+
+	public MapCoordinate Coordinate { get; }
+}
+
+public class Wall
+{
+    public MapCoordinate Coordinate { get; }
+    public WallState State { get; }
+
+    public Wall(MapCoordinate coordinate, WallState state)
+    {
+        Coordinate = coordinate;
+        State = state;
+    }
+}
+
+public enum WallState
+{
+	Broken,
+	Repaired
+}
+
+//public class WallIdentifier : IEquatable<WallIdentifier>
+//{
+//	private static int _nextId;
+//	private readonly int _id;
+//
+//	private WallIdentifier(int id)
+//	{
+//		_id = id;
+//	}
+//
+//	public static WallIdentifier Create()
+//		=> new WallIdentifier(_nextId++);
+//
+//	public override bool Equals(object obj)
+//		=> Equals(obj as WallIdentifier);
+//
+//	public bool Equals(WallIdentifier other)
+//		=> other != null && _id == other._id;
+//
+//	public override int GetHashCode()
+//		=> 1969571243 + _id.GetHashCode();
+//
+//	public static bool operator ==(WallIdentifier left, WallIdentifier right)
+//		=> EqualityComparer<WallIdentifier>.Default.Equals(left, right);
+//
+//	public static bool operator !=(WallIdentifier left, WallIdentifier right)
+//		=> !(left == right);
+//}
+
+public abstract class WallsEvent
+{
+	public class Initialized : WallsEvent
+	{
+		public Initialized(params InitialWall[] Walls)
+		{
+			Walls = Walls;
+		}
+
+		public IEnumerable<InitialWall> Walls { get; }
+	}
+
+	public class WallRepaired : WallsEvent
+	{
+        public MapCoordinate Coordinate { get; }
+
+        public WallRepaired(MapCoordinate coordinate)
+        {
+            Coordinate = coordinate;
+        }
+	}
+}
---
+++ Assets/Code/Integration/InitializeWallsWhenMapInitialized.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UniRx;
+
+public class InitializeWallsWhenMapInitialized
+{
+    public InitializeWallsWhenMapInitialized(MapAggregate map, WallsAggregate walls)
+    {
+        map.Events
+            .OfType<MapEvent, MapEvent.Initialized>()
+            .Subscribe(initialized => InitilizeWalls(initialized, walls));
+    }
+
+    private void InitilizeWalls(MapEvent.Initialized initialized, WallsAggregate walls)
+    {
+        var mapCoords = Enumerable.Range(0, initialized.MapCells.GetLength(0)).SelectMany(x => Enumerable.Range(0, initialized.MapCells.GetLength(1)).Select(y => new MapCoordinate(x, y)));
+
+        var initialWalls = mapCoords
+            .Where(coord => initialized.MapCells[coord.X, coord.Y] is WallCell)
+            .Select(coord => new InitialWall(coord))
+            .ToArray();
+
+        walls.Initialize(initialWalls);
+    }
+}
+public class RepairWallsWhenMapCellClicked
+{
+    public RepairWallsWhenMapCellClicked(MapAggregate map, WallsAggregate walls)
+    {
+        map.Events
+            .OfType<MapEvent, MapEvent.MapCellClicked>()
+            .Subscribe(clicked => RepairWalls(clicked, walls));
+    }
+
+    private void RepairWalls(MapEvent.MapCellClicked clicked, WallsAggregate walls)
+    {
+        walls.Repair(clicked.Coordinate);
+    }
+}
