a3df2a82a5016609e29a5804bb194556a75679ec
Andrew Kuipers <deimors@gmail.com>
2020-02-01 11:45:13 -0600

    Sketched out TowersAggregate

---
+++ Assets/Code/Domain/Towers/TowersAggregate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class TowersAggregate
+{
+	private Subject<TowersEvent> _events = new Subject<TowersEvent>();
+
+	public IObservable<TowersEvent> Events => _events;
+
+	public void Initialize(params InitialTower[] towers)
+	{
+		Emit(new TowersEvent.Initialized(towers));
+	}
+
+	private void Emit(TowersEvent @event)
+		=> _events.OnNext(@event);
+}
+
+public class InitialTower
+{
+	public InitialTower(TowerIdentifier towerId)
+	{
+		TowerId = towerId;
+	}
+
+	public TowerIdentifier TowerId { get; }
+}
+
+public class TowerIdentifier : IEquatable<TowerIdentifier>
+{
+	private static int _nextId;
+	private readonly int _id;
+
+	private TowerIdentifier(int id)
+	{
+		_id = id;
+	}
+
+	public static TowerIdentifier Create()
+		=> new TowerIdentifier(_nextId++);
+
+	public override bool Equals(object obj)
+		=> Equals(obj as TowerIdentifier);
+
+	public bool Equals(TowerIdentifier other)
+		=> other != null && _id == other._id;
+
+	public override int GetHashCode()
+		=> 1969571243 + _id.GetHashCode();
+
+	public static bool operator ==(TowerIdentifier left, TowerIdentifier right)
+		=> EqualityComparer<TowerIdentifier>.Default.Equals(left, right);
+
+	public static bool operator !=(TowerIdentifier left, TowerIdentifier right)
+		=> !(left == right);
+}
+
+public abstract class TowersEvent
+{
+	public class Initialized : TowersEvent
+	{
+		public Initialized(params InitialTower[] towers)
+		{
+			Towers = towers;
+		}
+
+		public IEnumerable<InitialTower> Towers { get; }
+	}
+}
