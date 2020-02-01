9b92e4df675c7fa12ef3517e1436b1a471396ead
Tristan Heisler <tristan.heisler@brewninja.net>
2020-02-01 11:00:37 -0600

    Added more towers types and generation thresholds.

--- Assets/Code/Domain/Map/Map.cs
+++ Assets/Code/Domain/Map/Map.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Map : MonoBehaviour
 {
-    public int BoardSize;
+    private readonly int BOARD_SIZE = 20;
 
     [SerializeField] private GameObject ground;
-    [SerializeField] private GameObject tower;
-    [SerializeField] private GameObject wall;
+    
+    [SerializeField] private GameObject brokenBasicTower;
+    [SerializeField] private GameObject brokenFireTower;
+    [SerializeField] private GameObject brokenIceTower;
+    [SerializeField] private GameObject brokenPlasmaTower;
+
+    [SerializeField] private GameObject brokenWall;
+
+    private struct TileThreshold
+    {
+        public float Threshold;
+        public GameObject Tile;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,32 +34,33 @@ public class Map : MonoBehaviour
 
     private void CreateLevel()
     {
+        TileThreshold[] tileThresholds = new TileThreshold[]{
+            new TileThreshold() { Threshold = 0.05f, Tile = brokenBasicTower},
+            new TileThreshold() { Threshold = 0.10f, Tile = brokenIceTower},
+            new TileThreshold() { Threshold = 0.15f, Tile = brokenFireTower},
+            new TileThreshold() { Threshold = 0.20f, Tile = brokenPlasmaTower},
+            new TileThreshold() { Threshold = 0.85f, Tile = brokenWall},
+            new TileThreshold() { Threshold = 1.00f, Tile = ground},
+        };
+
         var map = GetComponent<Map>().transform;
 
-        for (var x = 0; x < BoardSize; x++)
+        for (var x = 0; x < BOARD_SIZE; x++)
         {
-            for (var y = 0; y < BoardSize; y++)
+            for (var y = 0; y < BOARD_SIZE; y++)
             {
-                var tile = GetRandomTile();
+                var tile = GetRandomTile(tileThresholds);
 
                 Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity, map);
             }
         }
     }
 
-    private GameObject GetRandomTile()
+    private GameObject GetRandomTile(IEnumerable<TileThreshold> tileThresholds)
     {
-        var randomNumber = (int)(Random.value * 3);
+        var randomNumber = Random.value;
 
-        switch (randomNumber)
-        {
-            case 1:
-                return ground;
-            case 2:
-                return tower;
-            default:
-                return wall;
-        }
+        return tileThresholds.First(tileThreshold => randomNumber <= tileThreshold.Threshold).Tile;
     }
 
     // Update is called once per frame
