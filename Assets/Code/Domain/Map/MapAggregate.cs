e27762ae31830306cd9fdb039ed5ca41a24296c2
Andrew Kuipers <deimors@gmail.com>
2020-01-31 20:21:28 -0600

    Bones of MapAggregate

---
+++ Assets/Code/Domain/Map/MapAggregate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class MapAggregate
+{
+    private Subject<MapEvent> _events = new Subject<MapEvent>();
+
+    public IObservable<MapEvent> Event => _events;
+
+    public void Initialize(MapCell[,] mapCells)
+    {
+        Emit(new MapEvent.Initialized(mapCells));
+    }
+
+    private void Emit(MapEvent @event)
+        => _events.OnNext(@event);
+}
+
+public class MapCell
+{
+}
+
+public abstract class MapEvent
+{
+    public class Initialized : MapEvent
+    {
+        public Initialized(MapCell[,] mapCells)
+        {
+
+        }
+    }
+}
