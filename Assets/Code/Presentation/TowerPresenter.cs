b6ece7b4a1d2f87061a5ade39fe66e140ebf0eb8
Andrew Kuipers <deimors@gmail.com>
2020-01-31 21:01:34 -0600

    Tower firing bullet at enemy

--- Assets/Code/Presentation/TowerPresenter.cs
+++ Assets/Code/Presentation/TowerPresenter.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class TowerPresenter : MonoBehaviour
 {
-    private void OnTriggerStay2D(Collider2D collision)
+    public GameObject BulletPrefab;
+
+    public float BulletVelocity = 40f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log($"Collision at {collision.transform.position}");
+
+        var toCollision = (collision.transform.position - transform.position).normalized;
+
+        var bullet = GameObject.Instantiate(BulletPrefab);
+
+        var bulletRigidBody = bullet.GetComponent<Rigidbody2D>();
+
+        bulletRigidBody.velocity = toCollision * BulletVelocity;
     }
 }
