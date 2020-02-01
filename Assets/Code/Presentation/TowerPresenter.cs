4fd058dbf99152a66a9e2fc9b9ccc622b32a72e7
Andrew Kuipers <deimors@gmail.com>
2020-01-31 20:43:38 -0600

    Basic TowerPresenter trigger

---
+++ Assets/Code/Presentation/TowerPresenter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPresenter : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log($"Collision at {collision.transform.position}");
+    }
+}
