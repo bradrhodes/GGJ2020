eb1e20828658c00d1bf0f2472d8e50a69df1f695
JiawenLi08 <1556453954@qq.com>
2020-02-01 00:46:36 -0600

    Added a Flame tower

---
+++ Assets/Code/Presentation/FlamePresenter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamePresenter : MonoBehaviour
+{
+    // Update is called once per frame
+
+    float lifeTime = 1;
+    void Start()
+    {
+        StartCoroutine(WaitThenDie());
+    }
+    IEnumerator WaitThenDie()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Code/Presentation/FlameTowerPresenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameTowerPresenter : MonoBehaviour
+{
+    public GameObject FlamePrefab;
+
+    public float FlameVelocity = 40f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"Collision at {collision.transform.position}");
+
+        var toCollision = (collision.transform.position - transform.position).normalized;
+
+            var Flame = GameObject.Instantiate(FlamePrefab);
+
+            var FlameRigidBody = Flame.GetComponent<Rigidbody2D>();
+
+            FlameRigidBody.velocity = toCollision * FlameVelocity;
+
+    }
+
+}
+
