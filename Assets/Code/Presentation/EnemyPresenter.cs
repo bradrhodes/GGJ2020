58db7e420185588f8ba80a10298d64e7a2a701ce
Andrew Kuipers <deimors@gmail.com>
2020-01-31 20:32:56 -0600

    Basic EnemyPresenter

---
+++ Assets/Code/Presentation/EnemyPresenter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPresenter : MonoBehaviour
+{
+    public float Velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Velocity * transform.up * Time.deltaTime;
+    }
+}
