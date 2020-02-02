2144095daf5248de75a0a96a8e233c6f9ce1710f
Brad R <1196817+bradrhodes@users.noreply.github.com>
2020-02-01 22:05:20 -0600

    Created the tiles aggregate to track which tiles are occupied and publish events indicating so

---
+++ Assets/Code/Domain/Tiles/TilesAggregate.cs
@@ -0,0 +1,59 @@
+﻿using UniRx;
+
+public class TilesAggregate
+{
+    public Subject<TileEvent> _events = new Subject<TileEvent>();
+    private bool[,] _occupiedTiles = new bool[0,0];
+
+    public TilesAggregate()
+    {
+        
+    }
+
+    public void Initialize(int xDimension, int yDimension)
+    {
+        _occupiedTiles = new bool[xDimension, yDimension];
+        var outArray = new bool[xDimension, yDimension];
+        for (var x = 0; x < xDimension; x++)
+        for (var y = 0; y < yDimension; y++)
+        {
+            _occupiedTiles[x, y] = false;
+            outArray[x, y] = false;
+        }
+
+        Emit(new TileEvent.TilesInitialized(outArray));
+    }
+
+    public void SetTileAsOccupied(MapCoordinate coordinate)
+    {
+        _occupiedTiles[coordinate.X, coordinate.Y] = true;
+        Emit(new TileEvent.TileOccupied(coordinate));
+    }
+
+    private void Emit(TileEvent @event)
+		=> _events.OnNext(@event);
+}
+
+
+public abstract class TileEvent
+{
+    public class TilesInitialized : TileEvent
+    {
+        public bool[,] OccupiedTiles { get; }
+
+        public TilesInitialized(bool[,] occupiedTiles)
+        {
+            OccupiedTiles = occupiedTiles;
+        }
+    }
+
+    public class TileOccupied : TileEvent
+    {
+        public MapCoordinate Coordinate { get; }
+
+        public TileOccupied(MapCoordinate coordinate)
+        {
+            Coordinate = coordinate;
+        }
+    }
+}
---
+++ Assets/Code/Integration/TilesInstaller.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Code.Integration;
+using Zenject;
+
+public class TilesInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<TilesAggregate>().AsSingle();
+        Container.Bind<KeepTrackOfOccupiableCells>().AsSingle().NonLazy();
+    }
+}
