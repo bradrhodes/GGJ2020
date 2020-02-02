d5c177a692a79e0ee06fedf380c55b2e20976181
Andrew Kuipers <deimors@gmail.com>
2020-02-02 11:59:27 -0600

    Tracking and repeated fire

--- Assets/Code/Presentation/TowerPresenter.cs
+++ Assets/Code/Presentation/TowerPresenter.cs
@@ -4,16 +4,21 @@ using Zenject;
 
 public class TowerPresenter : MonoBehaviour
 {
-    [SerializeField] private GameObject brokenBaseSprite;
-    [SerializeField] private GameObject baseSprite;
-    [SerializeField] private GameObject turretSprite;
-    [SerializeField] private GameObject detectorSprite;
+	[SerializeField] private GameObject brokenBaseSprite;
+	[SerializeField] private GameObject baseSprite;
+	[SerializeField] private GameObject turretSprite;
+	[SerializeField] private GameObject detectorSprite;
 
 	public GameObject BulletPrefab;
 
 	public float BulletVelocity = 40f;
 
-    [Inject]
+	private EnemyIdentifier _target;
+
+	private float _reloadTime = 1f;
+	private float _remainingReloadTime;
+
+	[Inject]
 	public TowerParameters Parameters { private get; set; }
 
 	[Inject]
@@ -26,12 +31,12 @@ public class TowerPresenter : MonoBehaviour
 	{
 		transform.position = new Vector3(Parameters.Coordinate.X, Parameters.Coordinate.Y, 0);
 
-        ShowBrokenTower();
+		ShowBrokenTower();
 
-        Towers.Events
-            .OfType<TowersEvent, TowersEvent.TowerRepaired>()
-            .Where(repaired => repaired.Identifier == Parameters.TowerId)
-            .Subscribe(repaired => ShowRepairedTower());
+		Towers.Events
+			.OfType<TowersEvent, TowersEvent.TowerRepaired>()
+			.Where(repaired => repaired.Identifier == Parameters.TowerId)
+			.Subscribe(repaired => ShowRepairedTower());
 
 		Towers.Events
 			.OfType<TowersEvent, TowersEvent.EnemyTargetted>()
@@ -44,42 +49,63 @@ public class TowerPresenter : MonoBehaviour
 			.Subscribe(enemyUntargetted => UntargetEnemy(enemyUntargetted.EnemyId));
 	}
 
-    private void ShowBrokenTower()
-    {
-        brokenBaseSprite.SetActive(true);
+	private void ShowBrokenTower()
+	{
+		brokenBaseSprite.SetActive(true);
 		baseSprite.SetActive(false);
-        turretSprite.SetActive(false);
-        detectorSprite.SetActive(false);
+		turretSprite.SetActive(false);
+		detectorSprite.SetActive(false);
 	}
 
-    private void ShowRepairedTower()
-    {
-        brokenBaseSprite.SetActive(false);
-        baseSprite.SetActive(true);
-        turretSprite.SetActive(true);
-        detectorSprite.SetActive(true);
-    }
+	private void ShowRepairedTower()
+	{
+		brokenBaseSprite.SetActive(false);
+		baseSprite.SetActive(true);
+		turretSprite.SetActive(true);
+		detectorSprite.SetActive(true);
+	}
 
 	private void TargetEnemy(EnemyIdentifier enemyId)
 	{
-		FireBulletAt(enemyId);
+		_target = enemyId;
+		//FireBulletAt(enemyId);
 	}
 
 	private void UntargetEnemy(EnemyIdentifier enemyId)
 	{
-
+		if (_target == enemyId)
+			_target = null;
 	}
 
 	private void FireBulletAt(EnemyIdentifier enemyId)
-    {
-        var toCollision = (EnemyPositions[enemyId] - transform.position).normalized;
-        turretSprite.transform.rotation = Quaternion.LookRotation(Vector3.forward, toCollision);
+	{
+		var toEnemy = (EnemyPositions[enemyId] - transform.position).normalized;
+		//turretSprite.transform.rotation = Quaternion.LookRotation(Vector3.forward, toEnemy);
 
-        var bullet = Instantiate(BulletPrefab);
+		var bullet = Instantiate(BulletPrefab);
 		bullet.transform.position = transform.position;
 
 		var bulletRigidBody = bullet.GetComponent<Rigidbody2D>();
 
-		bulletRigidBody.velocity = toCollision * BulletVelocity;
+		bulletRigidBody.velocity = toEnemy * BulletVelocity;
+	}
+
+	private void Update()
+	{
+		if (_remainingReloadTime > 0)
+			_remainingReloadTime -= Time.deltaTime;
+
+		if (_target is null)
+			return;
+
+		var toEnemy = (EnemyPositions[_target] - transform.position).normalized;
+		turretSprite.transform.rotation = Quaternion.LookRotation(Vector3.forward, toEnemy);
+
+		if (_remainingReloadTime <= 0)
+		{
+			_remainingReloadTime = _reloadTime;
+
+			FireBulletAt(_target);
+		}
 	}
 }
