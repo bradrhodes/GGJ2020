9273f0dc03c3de09db69da21b31c82c19c3c8d76
Andrew Kuipers <deimors@gmail.com>
2020-02-01 19:28:36 -0600

    Goal Cell

---
+++ Assets/Code/Presentation/GoalCellPresenter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class GoalCellPresenter : MonoBehaviour
+{
+	[Inject]
+	public MapAggregate Map { private get; set; }
+
+	private void Start()
+	{
+		Map.Events
+			.OfType<MapEvent, MapEvent.Initialized>()
+			.Subscribe(initialized => InitializeGoalCell(initialized.MapCells));
+	}
+
+	private void InitializeGoalCell(MapCell[,] mapCells)
+	{
+		var goalCoord = FindGoalCoord(mapCells);
+
+		transform.position = new Vector3(goalCoord.X, goalCoord.Y, 0);
+	}
+
+	private MapCoordinate FindGoalCoord(MapCell[,] mapCells)
+	{
+		for (int x = 0; x < mapCells.GetLength(0); x++)
+		{
+			for (int y = 0; y < mapCells.GetLength(1); y++)
+			{
+				if (mapCells[x, y] is GoalCell)
+				{
+					return new MapCoordinate(x, y);
+				}
+			}
+		}
+
+		throw new InvalidOperationException("No Goal Cell Found!");
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log($"Goal cell triggered by {collision.gameObject.name}");
+	}
+}
